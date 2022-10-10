be9ff7b9bcca2117ae48396333fa5f80c31132d6
yanaleso <ktcmjzyfa@gmail.com>
2022-10-10 19:30:53 +0300

    Add homework

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,13 +1,13 @@
 const {Contact} = require("../../models/contact")
 const { RequestError } = require("../../helpers")
 
-const removeContact = async (req, res) => {
+const updateStatusContact = async (req, res) => {
   const { contactId } = req.params
-  const result = await Contact.findByIdAndRemove(contactId)
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true})
   if (!result) {
     throw RequestError(404, "Not found")
   }
-  res.status(200).json({message: "contact deleted"})
+  res.status(200).json(result)
 }
 
-module.exports = removeContact
+module.exports = updateStatusContact
